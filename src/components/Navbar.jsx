44223e740ebfed2194465fdee1f5c47ac561d436
unknown <prakar57@gmail.com>
2022-09-02 20:43:19 +0530

    add home link in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,18 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import {FormControl,Select,MenuItem} from '@mui/material';
+import {Link} from '@mui/material';
+import {useNavigate} from "react-router-dom"
 
 function Navbar({currencyChangeHandler,sx}) {
-
+  let navigate= useNavigate();
   return (
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+          <Link   component="button" onClick={()=>navigate(`/`)} sx={{color:"white" ,}}>
             Crypto-Watch
+            </Link>
           </Typography>
           <FormControl>
           <Select onChange={currencyChangeHandler} defaultValue="usd">
